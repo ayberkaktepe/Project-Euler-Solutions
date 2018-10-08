ac89aeeb960116d093889111070d085c4453ffe2
ayberkaktepe <ayberkaktepe@outlook.com>
2018-10-08 23:54:06 +0300

    for loop limit fixed

--- Problem001.java
+++ Problem001.java
@@ -4,7 +4,7 @@ public class Problem001 {
 	public static void main(String[] args) {
 		
 		int sum=0;
-		for(int i=1; i<=1000; i++) {
+		for(int i=1; i<1000; i++) {
 			if(i%3==0||i%5==0) {
 				sum+=i;
 			}
